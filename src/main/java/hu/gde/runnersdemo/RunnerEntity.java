dc5906a14674af3bc08b0173899baf0a032edb9c
Daniel <d.suszter@gmail.com>
2023-05-25 20:18:53 +0200

    sponsor entity letrehozase

--- src/main/java/hu/gde/runnersdemo/RunnerEntity.java
+++ src/main/java/hu/gde/runnersdemo/RunnerEntity.java
@@ -16,6 +16,8 @@ public class RunnerEntity {
     @OneToMany(mappedBy = "runner", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<LapTimeEntity> laptimes = new ArrayList<>();
 
+    @ManyToOne
+    private SponsorEntity sponsor;
 
     public RunnerEntity() {
     }
---
+++ src/main/java/hu/gde/runnersdemo/SponsorEntity.java
@@ -0,0 +1,34 @@
+package hu.gde.runnersdemo;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class SponsorEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long sponsorId;
+    private String sponsorName;
+
+    @JsonIgnore
+    @OneToMany
+    private List<RunnerEntity>runners=new ArrayList<>();
+
+    public long getSponsorId() {
+        return sponsorId;
+    }
+
+    public void setSponsorId(long sponsorId) {
+        this.sponsorId = sponsorId;
+    }
+
+    public String getSponsorName() {
+        return sponsorName;
+    }
+
+    public void setSponsorName(String sponsorName) {
+        this.sponsorName = sponsorName;
+        }
+    }
